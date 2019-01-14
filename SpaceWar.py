0af2d403e7b7a7b82f68fd46bbe1cc8c34f23abd
Lukáš Lachnit <31956776+Lachtyyy@users.noreply.github.com>
2019-01-14 22:31:58 +0100

    Update SpaceWar.py

--- SpaceWar.py
+++ SpaceWar.py
@@ -1,14 +1,14 @@
-#!/usr/bin/env python3
-# Soubor:  kameny.py
-# Datum:   06.11.2018 10:01
-# Autor:   Marek Nožka, nozka <@t> spseol <d.t> cz
-# Licence: GNU/GPL
-############################################################################
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 14 20:00:12 2019
+
+@author: lachtyyy
+"""
+
 import pyglet
 import random
-from random import randint
-import glob
 from math import sin, cos, radians, pi
+from pyglet.window.key import DOWN, UP, LEFT, RIGHT
 
 window = pyglet.window.Window(1000, 800)
 batch = pyglet.graphics.Batch()   # pro optimalizované vyreslování objektů
@@ -16,95 +16,121 @@ batch = pyglet.graphics.Batch()   # pro optimalizované vyreslování objektů
 
 class SpaceObject(object):
 
-    def __init__(self, img_file, x=None, y=None):
-
-        # nečtu obrázek
-        self.image = pyglet.image.load(img_file)
-        # střed otáčení dám na střed obrázku
+    def __init__(self,x=None, y=None,direction=None,speed=None, rspeed=None):
+        
+        num = random.choice(range(0, ))
+        self.image = pyglet.image.load('meteors/{}.png'.format(num))
         self.image.anchor_x = self.image.width // 2
         self.image.anchor_y = self.image.height // 2
-        # z obrázku vytvořím sprite
         self.sprite = pyglet.sprite.Sprite(self.image, batch=batch)
-        # pokud není atribut zadán vytvořím ho náhodně
-        self._x = x if x is not None else random.randint(0, window.width)
-        self._y = y if y is not None else random.randint(0, window.height)
-        # musím správně nastavit polohu sprite
+        self._x = x if x is not None else random.randint(600, window.width)
+        self._y = y if y is not None else random.randint(600, window.height)
         self.x = self._x
         self.y = self._y
-
-    @property
-    def x(self):
-        return self._x
-
-    @x.setter
-    def x(self, new):
-        self._x = self.sprite.x = new
-
-    @property
-    def y(self):
-        return self._y
-
-    @y.setter
-    def y(self, new):
-        self._y = self.sprite.y = new
-
-
-class Meteor(SpaceObject):
-
-    def __init__(self, x=None, y=None, direction=None,
-                 speed=None, rspeed=None):
-        y = window.height + 50
-        file_list = glob.glob('img/meteor*.png')
-        img_file = random.choice(file_list)
-        super().__init__(img_file, x, y)
-        self.speed = speed if speed is not None else randint(50, 200)
-        self.rspeed = rspeed if rspeed is not None else randint(- 40, 40)
-        self.direction = direction \
-            if direction is not None else randint(120, 240)
+        self.direction = direction if direction is not None else random.randint(0, 359)
+        self.speed = speed if speed is not None else random.randint(50, 200)
+        self.rspeed = rspeed if rspeed is not None else random.randint(-40, 40)
 
     def tick(self, dt):
-        # do promenne dt se uloží doba od posledního tiknutí
+        self.bounce()
+        
         self.x += dt * self.speed * cos(pi / 2 - radians(self.direction))
         self.sprite.x = self.x
         self.y += dt * self.speed * sin(pi / 2 - radians(self.direction))
         self.sprite.y = self.y
         self.sprite.rotation += 0.01 * self.rspeed
+        
+    def bounce(self):
+        rozmer = min(self.image.width, self.image.height)/2
+        if self.x + rozmer >= window.width:
+            self.direction = random.randint(190, 350)
+            return
+        if self.x - rozmer <= 0:
+            self.direction = random.randint(10, 170)
+            return
+        if self.y + rozmer >= window.height:
+            self.direction = random.randint(100, 260)
+            return
+        if self.y - rozmer <= 0:
+            self.direction = random.randint(-80, 80)
+            return
+    
+        
+class Raketa(object):
 
-
-class SpaceShip(SpaceObject):
     def __init__(self):
-        super().__init__('img/ship.png', x=200, y=200)
-
-    def tick(self):
-        pass
-
-
-class Meet():
-    meteors = []
-
-    def __init__(self, count):
-        for _ in range(count):
-            self.add_meteor()
-
-    def add_meteor(self, dt=None):
-        self.meteors.append(Meteor())
-
+        #načtu obrázek lodě
+        self.obrazek = pyglet.image.load("playerShip1_red.png")
+        #střed otáčení dáme do prostřed tohoto obrázku
+        self.obrazek.anchor_x = self.obrazek.width // 2
+        self.obrazek.anchor_y = self.obrazek.height // 2
+        #z obr. vytovoříme sprite
+        self.sprite =pyglet.sprite.Sprite(self.obrazek, batch=batch)
+        self.sprite.rotation = 50
+        self.speed = 350
+        self.x = 400
+        self.y = 200
+        self.sprite.x = self.x
+        self.sprite.y = self.y
+        
     def tick(self, dt):
-        for meteor in self.meteors:
-            meteor.tick(dt)
-            if meteor.x < 0 or meteor.y < 0 or meteor.y > window.width:
-                meteor.sprite.delete()
-                self.meteors.remove(meteor)
+        global keyboard
+        self.okraj()
+        for data in klavesy:
+            if data == LEFT:
+                self.sprite.rotation -= 5
+            if data == RIGHT:
+                self.sprite.rotation += 5
+            if data ==  UP:
+                self.x = self.x + self.speed*dt*sin(pi*self.sprite.rotation/180)
+                self.sprite.x = self.x
+                self.y = self.y + self.speed*dt*cos(pi*self.sprite.rotation/180)
+                self.sprite.y = self.y
+            if data == DOWN:
+                self.x = self.x + self.speed*dt-sin(pi*self.sprite.rotation/180)
+                self.sprite.x = self.x
+                self.y = self.y + self.speed*dt-cos(pi*self.sprite.rotation/180)
+                self.sprite.y = self.y
+         
+    def okraj(self):
+        rozmer = min(self.obrazek.width,self.obrazek.height)/2
+        if self.x + rozmer >= window.width+60:
+            self.sprite.x=-20
+        if self.x - rozmer < -60:
+            self.sprite.x=window.width+20
+        if self.y + rozmer >= window.height + 60:
+            self.sprite.y=-20
+        if self.y - rozmer < -60:
+            self.sprite.y=window.height+20
 
+klavesy=set()        
+for o in range(1):
+    lod=Raketa()
+    pyglet.clock.schedule_interval(lod.tiktak, 1/ 60 )
+
+for x in range(15):
+    kamen=SpaceObject()
+    pyglet.clock.schedule_interval(kamen.tick, 1/ 60 )
+
+
+
+
+
+@window.event
+def on_key_press(data, mod):
+    global klavesy
+    klavesy.add(data)
+    
+@window.event
+def on_key_release(data, mod):
+    global klavesy
+    klavesy.remove(data)
 
 @window.event
 def on_draw():
     window.clear()
     batch.draw()
-
-
-meet = Meet(10)
-ship = SpaceShip()
-pyglet.clock.schedule_interval(meet.tick, 1/30)
-pyglet.clock.schedule_interval(meet.add_meteor, 1/10)
-pyglet.app.run()
+ 
+    
+    
+pyglet.app.run()      
